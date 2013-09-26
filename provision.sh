1bb8e0b370afbebe21242ea00cdb8393d3a8f06b
Hunter Haugen <hunter@puppetlabs.com>
2013-09-26 10:23:36 -0700

    Update for pe 3.1.0 build

--- provision.sh
+++ provision.sh
@@ -21,7 +21,7 @@ if [ ! -d /opt/puppet ] ; then
 fi
 
 sed /etc/puppetlabs/puppet/puppet.conf -i -e 's^modulepath = /etc/puppetlabs/puppet/modules:/opt/puppet/share/puppet/modules^modulepath = /vagrant:/etc/puppetlabs/puppet/modules:/opt/puppet/share/puppet/modules^'
-mv /opt/puppet/share/puppet/modules/pe_mcollective /tmp
+#mv /opt/puppet/share/puppet/modules/pe_mcollective /tmp
 
 if [ $(hostname -s) = 'mcp' ] ; then
   /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production node:add name=m1 groups=puppet_master
@@ -32,8 +32,14 @@ if [ $(hostname -s) = 'mcp' ] ; then
   /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production node:variables name=m1 variables='activemq_brokers=m2'
   /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production node:variables name=m2 variables='activemq_brokers=m3'
   /opt/puppet/bin/puppet apply -e 'include pe_shared_ca::update_module'
-  service pe-puppet stop && service pe-httpd reload && sleep 10 && /opt/puppet/bin/puppet agent -t && sleep 10 && /opt/puppet/bin/puppet agent -t ; exit 0
-  echo -e 'pe-internal-dashboard\nmcp\nm1\nm2\nm3' >> /etc/puppetlabs/puppetdb/certificate-whitelist && /sbin/service pe-puppetdb restart
+  service pe-puppet stop && \
+    service pe-httpd reload && \
+    sleep 10 && \
+    /opt/puppet/bin/puppet agent -t && \
+    sleep 10 && \
+    /opt/puppet/bin/puppet agent -t && \
+    echo -e 'pe-internal-dashboard\nmcp\nm1\nm2\nm3' >> /etc/puppetlabs/puppetdb/certificate-whitelist && \
+    /sbin/service pe-puppetdb restart
 else
   /opt/puppet/bin/puppet apply /vagrant/pe_shared_ca/usage/is_ca_server.pp
   /opt/puppet/bin/puppet cert generate $(hostname -s) --dns_alt_names $(hostname -s),puppet && service pe-httpd start && /opt/puppet/bin/puppet agent -t ; exit 0
